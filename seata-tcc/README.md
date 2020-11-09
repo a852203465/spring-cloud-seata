0d32de8ce316f309537588555fa93ae74b4a076b
贾荣 <rong.jia@unionman.com.cn>
2020-11-09 17:02:35 +0800

    [BUG_ID] 无 [问题描述] 1. 增加分支 [提交人]贾荣

---
+++ seata-tcc/README.md
@@ -0,0 +1,4 @@
+# spring cloud 整合 seata 实现分布式事务 TCC模式
+
+
+
---
+++ seata-tcc/account-server/src/test/java/cn/darkjrong/accountserver/AccountServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.darkjrong.accountserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
